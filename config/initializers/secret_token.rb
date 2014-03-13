f757d2bf8deef495276dd3dcd3663b95e646ddc6
lamine kaba <lkaba2003@yahoo.com>
2014-03-13 01:31:30 -0500

    add db and change root page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MySecondApp::Application.config.secret_key_base = 'fa0944c5d199801edb243d08b0e98e007c33fb27669fdf799bb8b261f0be32966a005828a15c49d1ea24b9965586ededfd1e8cefce0b39b71cf55b00b6b82b63'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MySecondApp::Application.config.session_store :cookie_store, key: '_My_Second_App_session'
